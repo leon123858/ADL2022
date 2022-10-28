88cf00163e014447491236b8ae2ab587ea2334f8
leon123858 <a0970785699@gmail.com>
2022-10-29 00:24:29 +0800

    change pred set

--- HW2/report/run.sh
+++ HW2/report/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # command: bash ./run.sh /path/to/context.json /path/to/test.json  /path/to/pred/prediction.csv
-python train_select.py --max_seq_length=512 --pad_to_max_length --model_name_or_path='./model/basic-select' --tokenizer_name bert-base-chinese --do_predict --prediction_file "${2}" --context_file "${1}"  --output_dir ./cache --per_gpu_eval_batch_size=4 --per_device_train_batch_size=4 --overwrite_output
+python train_select.py --max_seq_length=512 --pad_to_max_length --model_name_or_path='./model/basic-select' --tokenizer_name bert-base-chinese --do_predict --prediction_file "${2}" --context_file "${1}"  --output_dir ./cache --per_gpu_eval_batch_size=2 --per_device_train_batch_size=2 --overwrite_output
 python middleprocess.py "${1}" "${2}"
 cd answerlib
-python run_qa.py --max_seq_length=512 --pad_to_max_length --model_name_or_path='../model/advance-answer' --tokenizer_name bert-base-chinese --do_predict --test_file='../cache/middle_test_result.json' --output_dir ../cache --per_gpu_eval_batch_size=4 --per_device_train_batch_size=4
+python run_qa.py --max_seq_length=512 --pad_to_max_length --model_name_or_path='../model/advance-answer' --tokenizer_name bert-base-chinese --do_predict --test_file='../cache/middle_test_result.json' --output_dir ../cache --per_gpu_eval_batch_size=2 --per_device_train_batch_size=2
 cd ..
 python postprocess.py "${3}"
