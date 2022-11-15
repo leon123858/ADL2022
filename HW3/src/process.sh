a2a1072411cbe44ffacf3f4089b3f237bf3c7299
leon123858 <a0970785699@gmail.com>
2022-11-15 13:35:46 +0800

    update setting

--- HW3/src/process.sh
+++ HW3/src/process.sh
@@ -2,7 +2,7 @@
 # command: bash ./process.sh /path/to/train.jsonl /path/to/dev.jsonl  /path/to/dir max_len
 
 python ../libs/summarization/run_summarization.py \
-    --model_name_or_path mt5-small \
+    --model_name_or_path google/mt5-small \
     --do_train \
     --do_eval \
     --train_file "${1}" \
@@ -12,4 +12,7 @@ python ../libs/summarization/run_summarization.py \
     --per_device_train_batch_size=2 \
     --per_device_eval_batch_size=2 \
     --overwrite_output_dir \
-    --max_source_length "${4}" \
+    --max_source_length 256 \
+    --max_target_length 64 \
+    --num_train_epochs 1 \
+    --gradient_accumulation_steps 2
