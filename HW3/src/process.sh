0e62e54238a02117755ca5726637f63b1b79343c
leon123858 <a0970785699@gmail.com>
2022-11-18 22:58:28 +0800

    update setting

--- HW3/src/process.sh
+++ HW3/src/process.sh
@@ -13,7 +13,7 @@ python ../libs/summarization/run_summarization.py \
     --per_device_train_batch_size=8 \
     --per_device_eval_batch_size=8 \
     --overwrite_output_dir \
-    --max_source_length 512 \
+    --max_source_length 256 \
     --max_target_length 64 \
     --text_column "text" \
     --summary_column "summary" \
