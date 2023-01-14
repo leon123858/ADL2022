d4327b934198da8588941957b34b97dc24939294
leon123858 <a0970785699@gmail.com>
2023-01-14 15:17:20 +0800

    debug of HW3

--- HW3/report/run.sh
+++ HW3/report/run.sh
@@ -3,10 +3,10 @@
 
 cd ./src
 # 調整輸入數據格式
-python ./preprocess.py -d "${1}" -t ../cache/test.json --no-ans
+python ./preprocess.py -d ../"${1}" -t ../cache/test.json --no-ans
 # 創建範例數據
 python ./preprocess.py -d ../data/public.jsonl -t ../cache/dev.json --ans
 # 運行測試腳本
 bash test.sh ../cache/test.json ../cache/dev.json ../cache ../model
 # 調整輸出格式
-python postprocess.py -o ../cache/test.json -t ../cache/generated_predictions.txt -d "${2}"
+python postprocess.py -o ../cache/test.json -t ../cache/generated_predictions.txt -d ../"${2}"
